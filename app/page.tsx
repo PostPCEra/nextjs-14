734397125804f5714e84de09f051b76bcbe7db7a
PostPcEra <asubbareddy@gmail.com>
2024-04-04 17:37:58 +0000

    commit 1 from terminal

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-left justify-between font-mono text-sm lg:flex">
         <h1>
           {" "}
-          ---left22-- Next.js version 14.1 simple site - update 333aaaaaaa
-          chrome. .
+          --- after gitub repo  2222-- Next.js version 14.1 simple site - update
+          333aaaaaaa chrome. .
         </h1>
         <h2> edit from firefox 12221 </h2>
         <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
