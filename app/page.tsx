7997858fc13420db65b38572b49fcd9a31a73977
PostPcEra <asubbareddy@gmail.com>
2024-04-04 17:24:54 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-left justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-left justify-between font-mono text-sm lg:flex">
+        <h1>
+          {" "}
+          ---left22-- Next.js version 14.1 simple site - update 333aaaaaaa
+          chrome. .
+        </h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+        <h1> Next.js version 14.1 simple site - update 333aaaaaaa chrome. .</h1>
+        <h2> edit from firefox 12221 </h2>
+      </div>
+    </main>
+  );
+}
